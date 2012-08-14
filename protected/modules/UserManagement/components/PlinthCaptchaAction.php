a9817c667c78ed5812acaaea72a77080c1e1db12
Ken McHugh <kmchugh@icatalyst.co.uk>
2012-08-14 23:59:36 +0800

    Update of Cpatcha

--- protected/modules/UserManagement/components/PlinthCaptchaAction.php
+++ protected/modules/UserManagement/components/PlinthCaptchaAction.php
@@ -12,22 +12,6 @@ class PlinthCaptchaAction extends CCaptchaAction
 	public function run()
 	{
 		@parent::run();
-		/*
-		if(isset($_GET[self::REFRESH_GET_VAR]))  // AJAX request for regenerating code
-		{
-			$code=$this->getVerifyCode(true);
-			echo CJSON::encode(array(
-				'hash1'=>$this->generateValidationHash($code),
-				'hash2'=>$this->generateValidationHash(strtolower($code)),
-				// we add a random 'v' parameter so that FireFox can refresh the image
-				// when src attribute of image tag is changed
-				'url'=>$this->getController()->createUrl($this->getId(),array('v' => uniqid())),
-			));
-		}
-		else
-			$this->renderImage($this->getVerifyCode());
-		Yii::app()->end();
-		 */
 	}
 }
 
