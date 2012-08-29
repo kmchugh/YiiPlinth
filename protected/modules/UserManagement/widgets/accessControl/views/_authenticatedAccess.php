2fdd1df616d029181a58924ff5c79bd51c0f4bcd
Ken McHugh <kmchugh@ICatalyst.co.uk>
2012-08-29 11:01:23 +0800

    Update to profile link

--- protected/modules/UserManagement/widgets/accessControl/views/_authenticatedAccess.php
+++ protected/modules/UserManagement/widgets/accessControl/views/_authenticatedAccess.php
@@ -1,6 +1,6 @@
 <ul>
     <li>
-        <a href="<?php echo Yii::app()->controller->createURL('/userProfile/update/guid/'.Yii::app()->user->GUID); ?>">
+        <a href="<?php echo Yii::app()->controller->createURL('/userProfile/view/guid/'.Yii::app()->user->GUID); ?>">
             <img class="thumb" src="<?php echo Yii::app()->user->getProfileImageURI(); ?>"/>
             <span class="userProfile"><?php echo Yii::app()->user->DisplayName; ?></span>
         </a>
