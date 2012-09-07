b5ee19a25804273dd0064d41857a7a09c75e720e
Ken McHugh <kmchugh@icatalyst.co.uk>
2012-09-07 14:25:48 +0800

    Updates for ajax support

--- protected/controllers/PlinthController.php
+++ protected/controllers/PlinthController.php
@@ -6,7 +6,9 @@
  */
 class PlinthController extends Controller
 {
-	// Initialise the controller, set the user language
+	/**
+	 * Initialises the Controller and sets the user language for this controller
+	 */
 	function init()
 	{
 		parent::init();
@@ -17,6 +19,51 @@ class PlinthController extends Controller
 			Yii::app()->session['_lang'] = $_POST['_lang'];
 		}
 		Yii::app()->language = isset(Yii::app()->session['_lang']) ?  Yii::app()->session['_lang'] : Yii::app()->request->getPreferredLanguage();
+	}
+
+	protected function isAjaxRequest()
+	{
+		return Yii::app()->request->isAjaxRequest;
+	}
+
+	protected function supportsAjaxRequest()
+	{
+		return true;
+	}
+
+	/**
+	 * Renders the view specified.  If this is an ajax request then the view will be rendered using renderPartial instead of 
+	 * render.  This allows for HTML injection
+	 * @param string $view name of the view to be rendered. See {@link getViewFile} for details
+	 * about how the view script is resolved.
+	 * @param array $data data to be extracted into PHP variables and made available to the view script
+	 * @param boolean $return whether the rendering result should be returned instead of being displayed to end users.
+	 * @return string the rendering result. Null if the rendering result is not required.
+	 * @see renderPartial
+	 * @see getLayoutFile
+	 */
+	public function render($tcView,$taData=null,$tlReturn=false)
+	{
+		if ($this->supportsAjaxRequest() && $this->isAjaxRequest())
+		{
+			if($this->beforeRender($tcView))
+			{
+				$lcOutput=$this->renderPartial($tcView,$taData,true);
+				$lcOutput=$this->processOutput($lcOutput);
+				if ($tlReturn)
+				{
+					return $lcOutput;
+				}
+				else
+				{
+					echo $lcOutput;
+				}
+			}
+		}
+		else
+		{
+			return parent::render($tcView, $taData, $tlReturn);
+		}
 	}
 }
 
