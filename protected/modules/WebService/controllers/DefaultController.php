14bfe377b5255ef53e049cf52b22ac68ab8c5c3c
Ken McHugh <kmchugh@icatalyst.co.uk>
2012-10-20 05:36:50 +0800

    Update for WS

--- protected/modules/WebService/controllers/DefaultController.php
+++ protected/modules/WebService/controllers/DefaultController.php
@@ -185,7 +185,7 @@ class DefaultController extends PlinthController
 				if (!$loStream->IsPrivate)
 				{
 					$loUserInfo = UserInfo::model()->findByAttributes('UserID', Yii::app()->user->ID);
-					$lcURL ='http://www.youcommentate.com/'.(is_null($loUserInfo) ? 'Stream/View/guid/'.$loStream->GUID : $loUserInfo->UserURL;
+					$lcURL ='http://www.youcommentate.com/'.(is_null($loUserInfo) ? 'Stream/View/guid/'.$loStream->GUID : $loUserInfo->UserURL);
 					// If the user has linked their Twitter account, TWEET!
 					$lcTweet = "I'm live on @YouCommentate, check me out at ".Utilities::shortenURL($lcURL).' - '.$loEvent->Title;
 					if (strlen($lcTweet) >= 140)
