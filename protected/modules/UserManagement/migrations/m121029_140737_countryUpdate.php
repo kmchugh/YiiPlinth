dd0c8a245acb541f26e2bec125233b07ac6d00e7
Ken McHugh <kmchugh@icatalyst.co.uk>
2012-10-29 23:50:27 +0800

    Update to countries migration

---
+++ protected/modules/UserManagement/migrations/m121029_140737_countryUpdate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Updates the country table to include ISO standard information
+ */
+class m121029_140737_countryUpdate extends CDbMigration
+{
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+}
