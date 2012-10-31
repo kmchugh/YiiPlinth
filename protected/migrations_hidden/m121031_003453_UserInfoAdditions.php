c95a7a683550b3189a12324d1db7cfa516cf0a3a
abhinavsahai88 <abhinav@youcommentate.com>
2012-10-31 15:41:48 +0800

    deleted userinfoadditions

--- protected/migrations_hidden/m121031_003453_UserInfoAdditions.php
+++ protected/migrations_hidden/m121031_003453_UserInfoAdditions.php
@@ -2,24 +2,26 @@
 
 class m121031_003453_UserInfoAdditions extends CDbMigration
 {
-	public function up()
-	{
-	}
-
-	public function down()
-	{
-		echo "m121031_003453_UserInfoAdditions does not support migration down.\n";
-		return false;
-	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
+		$this->addColumn('{{UserInfo}}','CountryID');
+		$this->addColumn('{{UserInfo}}','BirthDay');
+		$this->addColumn('{{UserInfo}}','BirthMonth');
+		$this->addColumn('{{UserInfo}}','BirthYear');
+		$this->addColumn('{{UserInfo}}','GenderID');
+		// We do this row by row so we can keep the migration code simple for multi platform
+		$laUsers = $this->getDbConnection()->createCommand()
+			->select('UserID, DisplayName, GUID')
+			->from('{{User}}')->queryAll();
 	}
 
 	public function safeDown()
 	{
+		$this->dropColumn('{{UserInfo}}','CountryID');
+		$this->dropColumn('{{UserInfo}}','BirthDay');
+		$this->dropColumn('{{UserInfo}}','BirthMonth');
+		$this->dropColumn('{{UserInfo}}','BirthYear');
+		$this->dropColumn('{{UserInfo}}','GenderID');
 	}
-	*/
+
 }
