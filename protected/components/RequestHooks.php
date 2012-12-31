7f4782bc51b81386ebf58e8418480122f14f0997
Ken McHugh <kmchugh@ICatalyst.co.uk>
2012-12-31 09:24:11 +0800

    Update for migrations

--- protected/components/RequestHooks.php
+++ protected/components/RequestHooks.php
@@ -4,16 +4,19 @@ class RequestHooks
 {
 	public function beginRequest(CEvent $toEvent)
 	{
-		if (isset(Yii::app()->session['theme']))
+		if (isset(Yii::app()->theme))
 		{
-			Yii::app()->theme = Yii::app()->session['theme'];
-		}
+			if (isset(Yii::app()->session['theme']))
+			{
+				Yii::app()->theme = Yii::app()->session['theme'];
+			}
 
-		if (isset($_REQUEST['requestType']) && $_REQUEST['requestType'] === 'mobile')
-		{
-			Yii::app()->theme = 'mobile';
+			if (isset($_REQUEST['requestType']) && $_REQUEST['requestType'] === 'mobile')
+			{
+				Yii::app()->theme = 'mobile';
+			}
+			Yii::app()->session['theme'] = Yii::app()->theme->name;
 		}
-		Yii::app()->session['theme'] = Yii::app()->theme->name;
 	}
 }
 
