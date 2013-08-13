8685cf8fe5e12d57203242b9833368a5ef9f3c29
Ken McHugh <kmchugh@ICatalyst.co.uk>
2013-08-13 18:09:25 +0800

    Test

---
+++ test/YIIPlinthTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kmchugh
+ * Date: 5/29/13
+ * Time: 2:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class YIIPlinthTest extends PHPUnit_Framework_TestCase {
+
+    public function testLoad()
+    {
+
+    }
+
+
+}
